ba1003b8b1b6c25c44c1f2794bd6408c7079c68f
Nicholas Miller <nimi2356@colorado.edu>
2024-09-29 16:14:15 -0600

    Added the edgar filterer

---
+++ Sponsor/EdgarFilterer.py
@@ -0,0 +1,36 @@
+# This code pulls all quarters of all years from the Edgar database and then filters it, extracting CIK, Company Name, Form Type, Date Filed, and Filename to a csv. 
+
+import pandas as pd
+import requests
+
+headers = {
+    "User-Agent" : "Nicholas Miller (nimi2356@colorado.edu)"
+}
+years = range(1993,2025)
+qtrs = ["QTR1", "QTR2", "QTR3", "QTR4"]
+
+data = {
+    'CIK': [],
+    'Company Name': [],
+    'Form Type': [],
+    'Date Filed': [],
+    'Filename': []
+}
+
+for year in years:
+    for qtr in qtrs:
+        print(year, qtr)
+        file_url = r"https://www.sec.gov/Archives/edgar/full-index/{}/{}/master.idx".format(year, qtr)
+        content = requests.get(file_url, headers=headers).content.decode("latin-1")
+        for line in content.splitlines():
+            if "10-K" in line:
+                elements = line.split('|')
+                assert(len(elements) == 5)
+                data['CIK'].append(elements[0])
+                data['Company Name'].append(elements[1])
+                data['Form Type'].append(elements[2])
+                data['Date Filed'].append(elements[3])
+                data['Filename'].append(elements[4])
+
+df = pd.DataFrame(data)
+df.to_csv("output.csv", index=False)
